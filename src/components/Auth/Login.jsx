bcd2c3cbf7907afbc742ac1dfb0b4c7cf45216e0
ssmp10 <ssmpamj10@gmail.com>
2026-09-07 21:21:10 +0530

    Log in process

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,44 +1,42 @@
 import React, { useState } from 'react'
 
-const Login = () => {
+const Login = ({handleLogin}) => {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const submitHandler = (e)=>{
+    const submitHandler = (e) => {
         e.preventDefault()
-        console.log("email is", email)
-        console.log("password is", password)
-
+        handleLogin(email, password)
         setEmail("")
         setPassword("")
     }
-  return (
-    <div className='flex h-screen w-screen items-center justify-center'>
-        <div className='border-2 p-20 border-emerald-600 rounded-xl'>
-            <form onSubmit={(e)=>{
-                submitHandler(e)
-            }} className='flex flex-col items-center justify-center'>
-                // EMAIL
-                <input
-                value = {email}
-                onChange={(e)=>{
-                    setEmail(e.target.value)
-                }}
-                required className='outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400' type='email' placeholder='Enter your email'/>
+    return (
+        <div className='flex h-screen w-screen items-center justify-center'>
+            <div className='border-2 p-20 border-emerald-600 rounded-xl'>
+                <form onSubmit={(e) => {
+                    submitHandler(e)
+                }} className='flex flex-col items-center justify-center'>
+                 {/* EMAIL */}
+                    <input
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value)
+                        }}
+                        required className='outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400' type='email' placeholder='Enter your email' />
 
-                // PASSWORD
-                <input
-                value={password}
-                onChange={(e)=>{
-                    setPassword(e.target.value)
-                }}
-                required className='outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full mt-3 placeholder:text-gray-400' type='password' placeholder='Enter password' />
-                <button className='text-white outline-none mt-5 border-none bg-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-white'>Log in</button>
-            </form>
+                 {/* PASSWORD */}
+                    <input
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value)
+                        }}
+                        required className='outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full mt-3 placeholder:text-gray-400' type='password' placeholder='Enter password' />
+                    <button className='text-white outline-none mt-5 border-none bg-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-white'>Log in</button>
+                </form>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Login
--- src/utils/localStorage.jsx
+++ src/utils/localStorage.jsx
@@ -306,4 +306,6 @@ export const setLocalStorage = () => {
 export const getLocalStorage = () => {
     const employees = JSON.parse(localStorage.getItem('employees'))
     const admin = JSON.parse(localStorage.getItem('admin'))
+
+    return {employees, admin}
 }
