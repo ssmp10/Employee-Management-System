4484060c6abe88b98a73a6ca4ebb73f1510fbe2a
ssmp10 <ssmpamj10@gmail.com>
2026-09-08 04:45:23 +0530

    EMS

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
 
   const handleLogin = (email, password) => {
     if (email == 'admin@me.com' && password == '123') {
-      setUser({ role: 'admin' })
+      setUser('admin')
       localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
     } else if (authData) {
       const employee = authData.employees.find((e) => email == e.email && e.password == password)
--- src/utils/localStorage.jsx
+++ src/utils/localStorage.jsx
@@ -1,4 +1,4 @@
-// localStorage.clear()
+//localStorage.clear()
 const employees = [
     {
         id: 1,
